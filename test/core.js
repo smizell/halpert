e82be34174a4a8e24e34480bc6046556fbc7bac2
Stephen Mizell <smizell@gmail.com>
2014-02-08 22:46:10 -0600

    Add tests for to function

--- test/core.js
+++ test/core.js
@@ -27,6 +27,25 @@ describe('Halpert Core', function() {
       var halObj = Halpert.convert(html).from('html').to('hal_json');
       expect(halObj).to.be.an(Object);
     })
+
+    it('should convert based on the initialData', function() {
+      var halObj = Halpert.convert(html).from('html')
+      var parsedData = halObj.parsedData;
+      var newHalObj = Halpert.convert(parsedData).to('hal_json');
+      expect(newHalObj).to.be.an(Object);
+    })
+
+    it('should not work if there is no initialData', function() {
+      Halpert.reset();
+      var halObj = Halpert.to('hal_json');
+      expect(halObj).to.equal(undefined);
+    })
+
+    it('should not work if the format is not registered', function() {
+      Halpert.reset();
+      var halObj = Halpert.to('not_registered');
+      expect(halObj).to.equal(undefined);
+    })
   })
 
   describe('reset', function() {
