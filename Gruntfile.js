efb4ec99c6ef3d00bdde44dbb97503dc2a614669
Stephen Mizell <smizell@gmail.com>
2014-03-15 21:27:58 -0500

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      all: ['Gruntfile.js', 'lib/*.js', 'test/*.js']
+      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/.js']
     },
     browserify: {
       dist: {
